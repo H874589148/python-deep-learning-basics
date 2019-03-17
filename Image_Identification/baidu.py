778fbefa0a108b43052c5f5870888dde78011b3f
Hawkeye <40380233+H874589148@users.noreply.github.com>
2019-03-17 16:20:35 +0800

    传图片识别图中植物 | GUI还没做

---
+++ Image_Identification/baidu.py
@@ -0,0 +1,32 @@
+import configparser                 #配置文件模块 读写配置文件
+
+from aip import AipImageClassify    #图像识别模块
+
+class BaiDuAPI(object):
+    #定义一个类
+    def __init__(self,filepath):
+        target = configparser.ConfigParser()
+        target.read(filepath,encoding='utf-8-sig')
+        app_id = target.get('password', 'APP_ID')
+        api_key = target.get('password', 'API_KEY')
+        secret_key = target.get('password', 'SECRET_KEY')
+
+        self.client = AipImageClassify(app_id,api_key,secret_key)
+
+    #读取图片
+    def get_file_content(self,filePath):
+        with open(filePath, 'rb') as fp:
+            return fp.read()
+
+    def picture2Name(self,filepath):
+        #读取图片
+        images = self.get_file_content(filepath)
+        alltexts = self.client.plantDetect(images)
+        text = alltexts.get('result', '')
+        #name = text.get('name','')
+        return text
+        #print(name)
+
+if __name__ == '__main__':
+    baiduapi = BaiDuAPI('passwd.ini')
+    print(baiduapi.picture2Name('plant.png'))
